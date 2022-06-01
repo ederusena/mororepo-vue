541db8f7f5bab41de43aa035ba01790fe09e9e75
Eder Sena <eder.sena@live.com>
2022-06-01 00:51:57 -0300

    teste monorepo

---
+++ packages/vue-ds/src/components/Button/Button.vue
@@ -0,0 +1,14 @@
+<script lang='ts'>
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class MyButton extends Vue {}
+</script>
+
+<template>
+  <button :class="$style.btn">I am button 2 - Press Me</button>
+</template>
+
+<style lang='scss' module>
+@import "~@ederusena/styles/src/components/Button.module.scss";
+</style>
